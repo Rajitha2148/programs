586c8fa074d74af80bd07e3eed8e8f823d115966
Rajitha2148 <52269899+Rajitha2148@users.noreply.github.com>
2019-07-01 11:32:28 +0530

    Update vowles.py

--- vowles.py
+++ vowles.py
@@ -1,5 +1,9 @@
 cha=input("enter your charac:")
 if(cha=='a' or cha=='e' or cha=='i' or cha=='o' or cha=='u' or cha=='A' or cha=='E' or cha=='I' or cha=='o' cha=='u'):
    print("the given charac",cha,"vowel")
-elif"
+elif(cha=='b' or cha=='c' or cha=='d' or cha=='g' or cha=='h' or cha=='j' or cha=='k' or cha=='l'
+     or cha=='m' or cha=='n' or cha=='p' or cha=='q' or char=='r' or cha=='s' or cha=='t'
+     or cha=='v' or cha=='w' or cha=='x' or cha=='y' or cha=='z'): 
    print("the given charac",cha,"constant")
+else:
+print("invalid")
