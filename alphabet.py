f4ea6e68f6bdbd8e75c0fdfe16709b7bb58b3574
Rajitha2148 <52269899+Rajitha2148@users.noreply.github.com>
2019-07-01 11:51:58 +0530

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,7 @@
+rd=input("enter your charactersss:")
+if((rd>='a' and rd<='z')(rd>='A' and rd>='Z')):
+  print(the charactersss",rd,"albhabet")
+elif(rd>='0' and rd<='9'):
+  print("the charactersss",rd,"not "albhabet")
+else:
+  print("invalid")
