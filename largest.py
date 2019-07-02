45d6c87d0ea458c48da3580d5c243d0f132acb13
Rajitha2148 <52269899+Rajitha2148@users.noreply.github.com>
2019-07-02 15:02:27 +0530

    Create largest.py

---
+++ largest.py
@@ -0,0 +1,10 @@
+numbe1=1
+numbe2=2
+numbe3=3
+if(numbe1 >= numbe2)and(numbe1 >= numbe3):
+largest=numbe1
+elif(numbe2 >= numbe1)and(numbe2 >= numbe3):
+largest=numbe2
+else:
+largest=numbe3
+print(("the larest numbe between",numbe1,",",numbe2,"and",numbe3,"is",largest))
