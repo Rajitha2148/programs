41b3e699fe08411a2acd94c5bde768dd8a573c4a
Rajitha2148 <52269899+Rajitha2148@users.noreply.github.com>
2019-07-05 15:36:26 +0530

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,7 +1,6 @@
-rd=input("enter your charactersss:")
-if((rd>='a' and rd<='z')(rd>='A' and rd>='Z')):
-  print(the charactersss",rd,"albhabet")
-elif(rd>='0' and rd<='9'):
-  print("the charactersss",rd,"not "albhabet")
-else:
-  print("invalid")
+dr=input()
+if(dr.isalnum()):
+  if(not dr.isdigit()):
+    print("Alphabet")
+  else:
+    print("no")
