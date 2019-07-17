979f314e87b117db5b5e7fa8c9cf0b78142cec9c
Rajitha2148 <52269899+Rajitha2148@users.noreply.github.com>
2019-07-17 15:05:05 +0530

    Update digits of an integer.py

--- digits of an integer.py
+++ digits of an integer.py
@@ -1,6 +1,11 @@
-ar=int(input("enter the number:"))
-count=0
-while(ar>0):
-   count=count+1
-   ar=ar/10
- print("the number of digits in the number are:",count)
+
+d,r=map(int,input().split())
+arr=[]
+arr=[int(i) for i in input().split()]
+i=0
+sum=0
+while r>0:
+  sum+=arr[i]
+  r=r-1
+  i=i+1
+print(sum)
