40355f472556b163e1d00d43998a9a5481a9fc4e
Rajitha2148 <52269899+Rajitha2148@users.noreply.github.com>
2019-07-17 15:27:06 +0530

    Update even no btwn 2 intervals.py

--- even no btwn 2 intervals.py
+++ even no btwn 2 intervals.py
@@ -1,4 +1,4 @@
-r,d=(map(int,input().split()))
-for j in range(r+1,d):
-  if i%2==0:
-    print(j,end=" ")
+s,d=map(int,input().split())
+for j in range(s+1,d):
+  if (j%2)==0:
+    print(j,end=' ')
