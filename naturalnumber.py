98b1285b252967e68eb7706333cd0e029f48b8d3
Rajitha2148 <52269899+Rajitha2148@users.noreply.github.com>
2019-07-17 14:56:37 +0530

    Update naturalnumber.py

--- naturalnumber.py
+++ naturalnumber.py
@@ -1,6 +1,4 @@
-dr=int(input("enter the numb:")
-value1=0
-while(dr > 0):
-value1=value1+dr
-dr=dr-1
-print("the sum of first  n natural number is",value1
+
+d=int(input()) 
+r=int((d*(d+1))/2)
+print(r) 
