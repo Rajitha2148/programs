06e8bf659c31c851bbeb48970b938005e52fd1f9
Rajitha2148 <52269899+Rajitha2148@users.noreply.github.com>
2019-07-26 08:14:27 -0700

    Update 3 with cost .py

--- 3 with cost .py
+++ 3 with cost .py
@@ -1,11 +1,11 @@
 dr,n=map(str,input().split())
 y=0
-if len(dr)>len(l1):
+if len(dr)>len(n):
   dr,n=n,dr
 bj=0
 while bj<len(dr):
   y+=(ord(n[bj])-ord(dr[bj]))
   bj+=1
-for bj in range(bj,len(l1)):
+for bj in range(bj,len(n)):
   y+=ord(n[bj])-ord('a')+1
 print(y)
